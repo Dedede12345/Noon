fe5586364f50c4a5efb6b31c5f39b5980b7afe51
Geb Den <gleb@adenisov.com>
2021-11-28 21:31:24 +0300

    inserted random

--- guessing.py
+++ guessing.py
@@ -1,4 +1,6 @@
-number = 78
+import random
+
+number = random.randint(1, 100)
 # Число для угадывания
 # 1. Запрос имя пользователя
 user = input('Введите ваше имя ')
@@ -7,4 +9,4 @@ guess = int(input(f'Введите число от 1 до 100, {user} '))
 # 3. Вывод «угадал» или «не угадал»
 victory = guess == number
 defeat = guess != number
-print('угадал'*victory, end='' 'Не угадал'*defeat)
+print('угадал'*victory, end='' f'Не угадал\nчисло равно {number}'*defeat)
